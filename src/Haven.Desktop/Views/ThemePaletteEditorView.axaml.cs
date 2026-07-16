e009f1566c964e44b74b63d3a999e99d41167bcb
CroakyJake12 <124530029+CroakyJake12@users.noreply.github.com>
2026-07-16 15:52:59 +0100

    Add manual palette editor host

---
+++ src/Haven.Desktop/Views/ThemePaletteEditorView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace Haven.Desktop.Views;
+
+public sealed partial class ThemePaletteEditorView : UserControl
+{
+    public ThemePaletteEditorView() => InitializeComponent();
+}
