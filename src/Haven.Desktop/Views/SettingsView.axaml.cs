ce725410d3f7d4617477a38e634b6299f0f416b3
CroakyJake12 <124530029+CroakyJake12@users.noreply.github.com>
2026-07-18 19:12:47 +0100

    Persist reduced motion from Settings

--- src/Haven.Desktop/Views/SettingsView.axaml.cs
+++ src/Haven.Desktop/Views/SettingsView.axaml.cs
@@ -1,13 +1,31 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using Haven.Desktop.Services;
 
 namespace Haven.Desktop.Views;
 
 public sealed partial class SettingsView : UserControl, IDisposable
 {
-    public SettingsView() => InitializeComponent();
+    private readonly MotionPreferencesService _motionPreferences;
+
+    public SettingsView()
+    {
+        InitializeComponent();
+        _motionPreferences = MotionPreferencesService.Current;
+        _motionPreferences.Changed += OnMotionPreferencesChanged;
+        ReduceAnimationsToggle.IsChecked = _motionPreferences.ReduceAnimations;
+    }
+
+    private void OnReduceAnimationsToggleClicked(object? sender, RoutedEventArgs e) =>
+        _motionPreferences.SetReduceAnimations(ReduceAnimationsToggle.IsChecked == true);
+
+    private void OnMotionPreferencesChanged(object? sender, EventArgs e) =>
+        Dispatcher.UIThread.Post(() => ReduceAnimationsToggle.IsChecked = _motionPreferences.ReduceAnimations);
 
     public void Dispose()
     {
+        _motionPreferences.Changed -= OnMotionPreferencesChanged;
         GenerativeUiSelector.Dispose();
         DataContext = null;
     }
