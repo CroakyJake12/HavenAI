0c06c3d269857a94a25af290a728fc0b8be09b7c
CroakyJake12 <124530029+CroakyJake12@users.noreply.github.com>
2026-07-27 22:55:01 +0100

    Remove the standalone top rail Tasks action

--- src/Haven.Desktop/Views/Shell/TopRail/TopRail.Tasks.cs
+++ src/Haven.Desktop/Views/Shell/TopRail/TopRail.Tasks.cs
@@ -1,11 +1,1 @@
-using Avalonia.Interactivity;
-
-namespace Haven.Desktop.Views.Shell.TopRail;
-
-public sealed partial class TopRail
-{
-    public event EventHandler? TasksRequested;
-
-    private void OnTasksClicked(object? sender, RoutedEventArgs e)
-        => TasksRequested?.Invoke(this, EventArgs.Empty);
-}
+// Haven Tasks is launched from the Apps list.
