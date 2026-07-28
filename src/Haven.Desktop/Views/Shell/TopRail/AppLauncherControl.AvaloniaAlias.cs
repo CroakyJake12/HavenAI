eae248e987e2d3b5fb32eb8ff111f80e291764b1
CroakyJake12 <124530029+CroakyJake12@users.noreply.github.com>
2026-07-28 10:15:22 +0100

    Resolve Avalonia application references in Tasks launcher

---
+++ src/Haven.Desktop/Views/Shell/TopRail/AppLauncherControl.AvaloniaAlias.cs
@@ -0,0 +1,9 @@
+namespace Haven.Desktop.Views.Shell.TopRail;
+
+public sealed partial class AppLauncherControl
+{
+    private static class Application
+    {
+        public static Avalonia.Application? Current => Avalonia.Application.Current;
+    }
+}
