23895c6297d9d26dac98cb21e46ff922b2b35a1b
CroakyJake12 <124530029+CroakyJake12@users.noreply.github.com>
2026-08-04 18:22:52 +0100

    Show Android runtime reports on resume

--- src/Haven.Android/MainActivity.cs
+++ src/Haven.Android/MainActivity.cs
@@ -1,5 +1,6 @@
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Avalonia.Android;
 
 namespace Haven.Android;
@@ -16,4 +17,33 @@ namespace Haven.Android;
         | ConfigChanges.KeyboardHidden)]
 public sealed class MainActivity : AvaloniaMainActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        try
+        {
+            base.OnCreate(savedInstanceState);
+            AndroidRuntimeDiagnostics.Attach(this);
+        }
+        catch (Exception exception)
+        {
+            AndroidRuntimeDiagnostics.Record(
+                exception,
+                "Android main activity startup",
+                showDialog: false);
+            AndroidRuntimeDiagnostics.ShowStartupToast(this);
+            throw;
+        }
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+        AndroidRuntimeDiagnostics.Attach(this);
+    }
+
+    protected override void OnPause()
+    {
+        AndroidRuntimeDiagnostics.Detach(this);
+        base.OnPause();
+    }
 }
